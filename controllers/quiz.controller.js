03275aa9d0464b7d800827c4d4b1e6ad9068fd6f
Aditya <adityabasak7031@gmail.com>
2025-01-30 19:54:24 +0530

    add business logic API

---
+++ controllers/quiz.controller.js
@@ -0,0 +1,32 @@
+import { fetchQuizSubmissionData } from "../utils/fetchData.js";
+
+const analyzePerformance = async (req, res) => {
+
+    try {
+
+        const submissionData = await fetchQuizSubmissionData();
+
+        const totalQuestions = submissionData.total_questions;
+        const correctAnswers = submissionData.correct_answers;
+        const incorrectAnswers = submissionData.incorrect_answers;
+
+        const accuracy = (correctAnswers / totalQuestions) * 100;
+
+
+        res.status(200).json({
+            totalQuestions,
+            correctAnswers,
+            incorrectAnswers,
+            accuracy: accuracy.toFixed(2) + "%",
+            rank: submissionData.rank_text
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Error analyzing quiz performance",
+            error: err.data
+        });
+    }
+}
+
+export default analyzePerformance;
