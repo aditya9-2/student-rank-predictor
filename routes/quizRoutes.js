0d9000588bde3ee31248676de24143578cbc75e6
Aditya <adityabasak7031@gmail.com>
2025-01-30 23:20:43 +0530

    add generateInsigs route

--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import analyzePerformance from "../controllers/quiz.controller.js";
 import predictRank from "../controllers/predictRank.controller.js";
+import generateInsights from "../controllers/generateInsights.controller.js";
 
 const router = express.Router();
 
 router.get("/analyze", analyzePerformance);
 router.get("/predict-rank", predictRank);
+router.get("/generate-insights", generateInsights);
 
 export default router
