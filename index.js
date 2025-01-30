88d471837c11ef1a68ccc229dc87a2687149b2cc
Aditya <adityabasak7031@gmail.com>
2025-01-30 19:55:12 +0530

    add v1

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
-app.use('/api/quiz', quizRoutes);
+app.use('/api/v1/quiz', quizRoutes);
 
 app.listen(PORT, () => {
     console.log(`app listens on ${PORT}`);
