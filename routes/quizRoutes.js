053395f600517a0ba72e2cfec6b0d2fc91c57b39
Aditya <adityabasak7031@gmail.com>
2025-01-30 20:10:50 +0530

    add predict-rank route

--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import analyzePerformance from "../controllers/quiz.controller.js";
+import predictRank from "../controllers/predictRank.controller.js";
 
 const router = express.Router();
 
 router.get("/analyze", analyzePerformance);
+router.get("/predict-rank", predictRank);
 
 export default router
